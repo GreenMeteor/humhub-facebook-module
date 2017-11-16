7e1b139e827d966af9945d9d2d83fb4e28592774
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-11-15 20:32:29 -0800

    Update facebookframe.php

--- widgets/views/facebookframe.php
+++ widgets/views/facebookframe.php
@@ -7,13 +7,18 @@ use humhub\models\Setting;
 \humhub\modules\facebook\Assets::register($this);
 ?>
 
-<div class="panel">
+<div class="panel panel-default panel-discordapp" id="panel-facebook">
+    <?= \humhub\widgets\PanelMenu::widget(['id' => 'panel-facebook']); ?>
   <div class="panel-heading">
-    <?= Yii::t('FacebookModule.base', '<strong>Discord</strong> Chat'); ?>
+    <?= Yii::t('FacebookModule.base', '<strong>Facebook</strong> Page'); ?>
   </div>
   <div class="panel-body">
 
 <?= Html::beginTag('div') ?>
-<iframe src="<?= $facebookUrl; ?>" width="100%" height="500" allowtransparency="true" frameborder="0" name="iframeContainer"></iframe>
+
+<iframe src="<? $facebook; ?>" width="100%" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+
+<?= Html::endTag('div'); ?>
+
 </div>
 </div>
