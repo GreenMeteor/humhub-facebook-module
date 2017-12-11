9caad60f57994b8a63cafee85056f2bec67079c2
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-12-12 02:02:49 +0900

    Update facebookframe.php

--- widgets/views/facebookframe.php
+++ widgets/views/facebookframe.php
@@ -23,7 +23,7 @@ use humhub\models\Setting;
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=159998961420330';
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId={id}';
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 <?= Html::endTag('div'); ?>
